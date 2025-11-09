459d0a104ab03907d7b216871960bc0c18c22bd9
Elliot <1073323+kurucu@users.noreply.github.com>
2025-11-09 22:17:25 +1100

    v1.0.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
 # Changelog
 
 All notable changes to `earhart` will be documented in this file.
+
+# v1.1.0
+- Added AuthAccountController to provide redirect to PropelAuth account manager.
+
+# v1.0.0
+- Initial version
---
+++ src/Controllers/AuthAccountController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LittleGreenMan\Earhart\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+
+class AuthAccountController
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        return redirect(config('services.propelauth.auth_url') . '/account');
+    }
+}
